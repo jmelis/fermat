69c33cb4d2710daf4c67ccec76f56d7094ef0c82
Pablo Guaza <pabloguaza@gmail.com>
2011-03-22 18:40:58 +0100

    jaime and pablo pushing the limits

--- web/javascripts/application.js
+++ web/javascripts/application.js
@@ -1,14 +1,35 @@
-var app={
+var GAME_DURATION = 600;
+var WINNING_QUESTIONS = 3;
+
+var timer = {};
+
+var questions=[
+	{
+		file: "acertijo2.ggb",
+		answer: 2002,
+		kind: "geogebra",
+		text: "En la pantalla de arriba puedes ver cuatro vistas diferentes de un mismo cubo. coloca la cara que falta en la cuarta vista con el color que le corresponde"
+	}
+];
+
+var question;
+
+var right_answers;
+
+
+
+var app = {
 	
 	gameBegin: function(){
-		$('#start-wrapper').fadeIn(4000);
 		$('#scroll-wrapper').hide();
 		$('#game-console-wrapper').hide();
+		$('#start-wrapper').fadeIn(4000);
 		
-		var counter = 0;
-		
+		var counter = 0;		
 		$('#start>img').everyTime(10,'initialScreen',function(){
 			counter -= 1;
+			if(counter ==-360)
+				counter = 0;
 			$('#start>img').css({
 			                        MozTransform: 'rotate(-' + -counter + 'deg)',
 			                        WebkitTransform: 'rotate(' + -counter + 'deg)',
@@ -44,9 +65,123 @@ var app={
 		$('#right-arrow').click(function() {
 			$('#scroll-wrapper').fadeOut(1000);
 			$('#game-console-wrapper').fadeIn(2000);
+			app.startGame();
 		});
-	}
+	},
 	
+	// http://sedition.com/perl/javascript-fy.html
+	prepareQuestionArray: function(){
+		var i = questions.length;
+		  if ( i == 0 ) return false;
+		  while ( --i ) {
+		     var j = Math.floor( Math.random() * ( i + 1 ) );
+		     var tempi = questions[i];
+		     var tempj = questions[j];
+		     questions[i] = tempj;
+		     questions[j] = tempi;
+		   }
+	},
+	
+	startGame: function(){
+		right_answers = 0;
+		timer.running = true;
+		timer.remaining = GAME_DURATION;
+		
+		$('#visual').html('');
+		$('#statement').html('');
+		
+		app.prepareQuestionArray();
+		app.loadQuestion();
+		app.startTimer();		
+		app.setupListeners();
+	},
+	
+	loadQuestion: function(){
+		question = questions.pop();
+		app.questionLoader(question);
+	},
+	
+	startTimer: function(){
+		// arduino START
+		
+		$('#webcam').everyTime(1000,'gameTimer',app.timerHandler);
+	},
+	
+	die: function(){
+		alert('LA HAS PALMAO');
+	},
+	
+	setupListeners: function(){
+		$('#fermat_submit').submit(function() {
+			if(question.answer == $('#code_text').value()){
+				
+				right_answers++;
+				if(right_answers == WINNING_QUESTIONS){
+					//arduino go back to initial position
+					app.win();
+				}
+				else{
+					//arduino stop 30s
+					app.loadQuestion();
+					setTimeout("app.reactivateTimer()",30000);
+				}
+					
+			}
+			else{
+				$('')
+			}
+				
+			return false;
+		});
+	},
+	// private
+	questionLoader: function(question) {
+		switch(question.kind)
+		{
+		case "geogebra":
+		  visual_code = '	<applet name="ggbApplet" code="geogebra.GeoGebraApplet" archive="questions/geogebra.jar"\
+				codebase="./"\
+				width="470" height="304"mayscript="true">\
+				<param name="filename" value="questions/' + question.file + '"/>\
+				<param name="java_arguments" value="-Xmx512m -Djnlp.packEnabled=true" />\
+				<param name="cache_archive" value="questions/geogebra.jar, questions/geogebra_main.jar, questions/geogebra_gui.jar, questions/geogebra_cas.jar, questions/geogebra_export.jar, questions/geogebra_properties.jar" />\
+				<param name="cache_version" value="3.2.46.0, 3.2.46.0, 3.2.46.0, 3.2.46.0, 3.2.46.0, 3.2.46.0" />\
+				<param name="framePossible" value="false" />\
+				<param name="showResetIcon" value="false" />\
+				<param name="showAnimationButton" value="true" />\
+				<param name="enableRightClick" value="false" />\
+				<param name="errorDialogsActive" value="true" />\
+				<param name="enableLabelDrags" value="false" />\
+				<param name="showMenuBar" value="false" />\
+				<param name="showToolBar" value="false" />\
+				<param name="showToolBarHelp" value="false" />\
+				<param name="showAlgebraInput" value="false" />\
+				<param name="allowRescaling" value="true" />\
+			</applet>';
+		  break;
+		case "plain":
+		  visual_code = '';
+		  break;
+		}
+
+		text = '<p>' + question.text + '</p>';
+
+		$('#visual').html(visual_code);
+		$('#statement').html(text);
+	},
+		
+	timerHandler: function(){
+		if(timer.running){
+			timer.remaining--;
+			if(timer.remaining<=0)
+				app.die();
+		}
+	},
+	
+	reactivateTimer: function(){
+		// arduino start
+		timer.running = true;
+	}
 };
 
 
