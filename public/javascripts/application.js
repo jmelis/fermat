a88f0ce51f5f35e8e3728b62ba586f9d3aaa2324
Jaime Melis <j.melis@fdi.ucm.es>
2011-03-26 19:24:14 +0100

    Placed Arduino JS API calls in the application. Handle timers.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,6 +4,7 @@ var WINNING_QUESTIONS = 3;
 var MAX_TRIES = 3;
 
 var timer = {};
+var win_timer;
 
 var questions=[
 	{
@@ -25,16 +26,14 @@ var question;
 var right_answers;
 var current_tries;
 
-
-
 var app = {
-	
+
 	gameBegin: function(){
 		$('#scroll-wrapper').hide();
 		$('#game-console-wrapper').hide();
 		$('#start-wrapper').fadeIn(4000);
-		
-		var counter = 0;		
+
+		var counter = 0;
 		$('#start>img').everyTime(10,'initialScreen',function(){
 			counter -= 1;
 			if(counter ==-360)
@@ -44,12 +43,12 @@ var app = {
 			                        WebkitTransform: 'rotate(' + -counter + 'deg)',
 			                        transform: 'rotate(' + -counter + 'deg)'
 			                    }
-								,100); 
+								,100);
 		});
-		
+
 		//ocultar calavera
 	},
-	
+
 	showScrollOnStartButtonClick: function(){
 		$('#button').click(function() {
 			$('#start-wrapper').fadeOut(1000, function(){
@@ -58,7 +57,7 @@ var app = {
 			$('#scroll-wrapper').fadeIn(2000, function(){
 				var scroll_p = $(this).find('p');
 				var timeBeetweenP = 2000;
-				
+
 				$(scroll_p[0]).fadeIn(1000, function(){
 					$(scroll_p[1]).delay(timeBeetweenP).fadeIn(1000, function(){
 						$(scroll_p[2]).delay(timeBeetweenP).fadeIn(1000);
@@ -68,7 +67,7 @@ var app = {
 			});
 		});
 	},
-	
+
 	showGameConsoleOnArrowClick: function(){
 		$('#right-arrow').click(function() {
 			$('#scroll-wrapper').fadeOut(1000);
@@ -76,7 +75,7 @@ var app = {
 			app.startGame();
 		});
 	},
-	
+
 	// http://sedition.com/perl/javascript-fy.html
 	prepareQuestionArray: function(){
 		var i = questions.length;
@@ -89,38 +88,39 @@ var app = {
 		     questions[j] = tempi;
 		   }
 	},
-	
+
 	startGame: function(){
 		right_answers = 0;
 		timer.running = true;
 		timer.remaining = GAME_DURATION;
-		
+
 		$('#visual').html('');
 		$('#statement').html('');
-		
+        $('#code-text').html('');
+
 		app.prepareQuestionArray();
 		app.loadQuestion();
-		app.startTimer();		
+		app.startTimer();
 		app.setupListeners();
 	},
-	
+
 	loadQuestion: function(){
 		current_tries = 0;
 		question = questions.pop();
 		app.questionLoader(question);
 	},
-	
+
 	startTimer: function(){
-		// arduino START
-		
+		Arduino.start();
+
 		$('#webcam').everyTime(1000,'gameTimer',app.timerHandler);
 	},
-	
+
 	die: function(){
 		alert('LA HAS PALMAO');
 		//parar timers
 	},
-	
+
 	setupListeners: function(){
 		$('#code-submit').click(function() {
 			// question answered right
@@ -134,12 +134,16 @@ var app = {
 				//player hasnt won yet
 				else{
 					//arduino stop 30s
+                    clearTimeout(win_timer);
+                    Arduino.stop();
+                    timer.running = false;
+
 					$('#information-display p').css({color:'green'}).html('¡Respuesta correcta! ' + (WINNING_QUESTIONS- right_answers) + ' más para ganar.').fadeIn(500).fadeOut(3000,function() {
 						$('#code-text').val("");
 						app.loadQuestion();
-						setTimeout("app.reactivateTimer()",30000);
-					});					
-				}		
+						win_timer = setTimeout("app.reactivateTimer()",30000-3500); //TBD: quitar chapuza tiempo
+					});
+				}
 			}
 			// question answered wrong
 			else{
@@ -149,30 +153,18 @@ var app = {
 					$('#information-display p').css({color:'red'}).html('¡Demasiados fallos, siguiente pregunta!').fadeIn(500).fadeOut(3000,function() {
 						$('#code-text').val("");
 						app.loadQuestion();
-						//ARDUINO: pone contador marcha atras de 30s por haber fallado
-					});					
+                        app.blockCodetext('¡Respuesta incorrecta! Penalización de 30 s');
+					});
 				}
 				// still trying
 				else{
-					$('#code-text').val("");
-					$('#information-display p').css({color:'red'}).html('¡Respuesta incorrecta! Penalización de 30 s').fadeIn(500).fadeOut(3000);
-					$('form').hide();
-					$('#progressbar').show();
-					
-					var stepDelay = 333;
-					
-					$('#progressbar').everyTime(stepDelay,'penaltyTimer',function(i) {
-						console.log(parseInt(100 - i/(300)*stepDelay));
-						$(this).progressbar({
-							value: parseInt(100 - i/(300)*stepDelay)
-						});
-					}, 30000/stepDelay);
+                    app.blockCodetext('¡Respuesta incorrecta! Penalización de 30 s');
 				}
-			}	
+			}
 			return false;
 		});
 	},
-	
+
 	// private
 	questionLoader: function(question) {
 		switch(question.kind)
@@ -208,7 +200,7 @@ var app = {
 		$('#visual').html(visual_code);
 		$('#statement').html(text);
 	},
-		
+
 	timerHandler: function(){
 		if(timer.running){
 			timer.remaining--;
@@ -216,16 +208,39 @@ var app = {
 				app.die();
 		}
 	},
-	
+
 	reactivateTimer: function(){
-		// arduino start
+		Arduino.start();
 		timer.running = true;
-	}
+	},
+
+    blockCodetext: function(msg) {
+        $('#code-text').val("");
+        $('#information-display p').css({color:'red'}).html(msg).fadeIn(500).fadeOut(3000);
+        $('form').hide();
+        $('#progressbar').show();
+
+        var stepDelay = 333;
+        var numberSteps = parseInt(30000/stepDelay);
+
+        $('#progressbar').everyTime(stepDelay,'penaltyTimer',function(i) {
+            $(this).progressbar({
+                value: parseInt(100 - 100*i/numberSteps)
+            });
+
+            // restore #code-text in last iteration
+            if (i == numberSteps){
+                $('form').show();
+                $('#progressbar').hide();
+                $('#information-display p').html('');
+            }
+        }, numberSteps);
+    }
 };
 
 
 $(document).ready(function(){
 	app.gameBegin();
 	app.showScrollOnStartButtonClick();
-	app.showGameConsoleOnArrowClick();
+	app.showGameConsoleOnArrowClick();*/
 });
