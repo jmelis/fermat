505da96be447c43f4bce5d43250d34412881e35e
Jaime Melis <j.melis@fdi.ucm.es>
2011-03-26 19:22:48 +0100

    Moved files to their final path

--- fermat.rb
+++ fermat.rb
@@ -6,18 +6,16 @@ require 'models/arduino'
 
 arduino = Arduino.new
 
-set :public, 'web'
-
 mime_type :ttf, "application/octet-stream"
 mime_type :woff, "application/octet-stream"
 mime_type :ggb, "application/octet-stream"
 
 get '/' do
-    File.read(File.dirname(__FILE__)+'/web/fermat.html')
+    File.read(File.dirname(__FILE__)+'/templates/fermat.html')
 end
 
 get '/questions/:question' do
-    File.read(File.dirname(__FILE__) + '/web/questions/' + params[:question])
+    File.read(File.dirname(__FILE__) + '/questions/' + params[:question])
 end
 
 # Arduino API
